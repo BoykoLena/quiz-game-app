cd7d1a190f3a4bc3e8f49fa57bea795360263eaa
BoykoLena <boykolena15@gmail.com>
2022-07-06 15:15:18 +0300

    add the project

---
+++ index.js
@@ -0,0 +1,348 @@
+let arr = [
+  {
+    question: "What was Meta Platforms Inc formerly known as?",
+    clue: "social networking site",
+    answer: "Facebook",
+  },
+  {
+    question: "Which English city is known as the Steel City?",
+    clue: "city in South Yorkshire",
+    answer: "Sheffield",
+  },
+  {
+    question: "Which former British colony was given back to China in 1997?",
+    clue: "city and special administrative region of China",
+    answer: "Hong Kong",
+  },
+  {
+    question: "The logo for luxury car maker Porsche features which animal?",
+    clue: "was taken from Stuttgart's Coat of Arms",
+    answer: "Horse",
+  },
+  {
+    question: "Which element is said to keep bones strong?",
+    clue: "chemical element with the symbol Ca",
+    answer: "Calcium",
+  },
+  {
+    question: "What does CIA stand for?",
+    clue: "the first line of defense for the United States",
+    answer: "Central Intelligence Agency",
+  },
+  {
+    question: "Suriname is located on which continent?",
+    clue: "continent entirely in the Western Hemisphere and mostly in the Southern Hemisphere",
+    answer: "South America",
+  },
+  {
+    question:
+      "In tennis, what piece of fruit is found at the top of the men’s Wimbledon trophy?",
+    clue: "tropical plant with an edible fruit",
+    answer: "Pineapple",
+  },
+  {
+    question: "Haematology is the study of what?",
+    clue: "body fluid",
+    answer: "Blood",
+  },
+  {
+    question: "What is the main ingredient in guacamole?",
+    clue: "bright green fruit with a large pit and dark leathery skin",
+    answer: "Avocado",
+  },
+  {
+    question: "Which body sets the interest rates in the UK?",
+    clue: "the UK's central bank",
+    answer: "Bank of England",
+  },
+  {
+    question: "How many lives is a cat said to have?",
+    clue: "number less than ten",
+    answer: "Nine",
+  },
+  {
+    question:
+      "Randall Flag, Jack Torrance and John Coffey are all characters created by which author?",
+    clue: "is an American author of horror",
+    answer: "Stephen King",
+  },
+  {
+    question: "Who founded the World Scouting Movement?",
+    clue: "soldier, national hero, educator and prolific writer",
+    answer: "Robert Baden-Powell",
+  },
+  {
+    question:
+      "In 2018, Scott Morrison was elected as the Prime Minister of which country?",
+    clue: "sovereign country comprising the mainland ",
+    answer: "Australia",
+  },
+  {
+    question: "What is a female elephant called?",
+    clue: "the male elephant is called a bull.",
+    answer: "Cow",
+  },
+  {
+    question: "Franz Josef Strauss Airport is located in which country?",
+    clue: "country in Central Europe",
+    answer: "Germany",
+  },
+  {
+    question:
+      "Which religion is based on the spiritual teaching of Guru Nanak?",
+    clue: "religion and philosophy founded in the Punjab region of the Indian subcontinent in the late 15th century",
+    answer: "Sikhism",
+  },
+  {
+    question: "On what date is Bastille Day celebrated in France?",
+    clue: "the French National Day is the anniversary of the Storming of the Bastille",
+    answer: "Fourteenth July",
+  },
+
+  {
+    question: "Which mountain overlooks Cape Town in South Africa?",
+    clue: "a flat-topped mountain forming a prominent landmark overlooking the city of Cape Town in South Africa",
+    answer: "Table Mountain",
+  },
+  {
+    question: "Caledonia was the Roman name for which modern day country?",
+    clue: "a country that is part of the United Kingdom",
+    answer: "Scotland",
+  },
+  {
+    question: "What brand of beer does Homer Simpson drink?",
+    clue: "a brand of beer that originated as a fictional beverage on the animated series The Simpsons",
+    answer: "Duff Beer",
+  },
+  {
+    question: "Rojo is the Spanish word for which colour?",
+    clue: "color of blood",
+    answer: "Red",
+  },
+  {
+    question: "How many timezones are there in mainland USA?",
+    clue: "they are Eastern, Central, Mountain & Pacific",
+    answer: "Four",
+  },
+  {
+    question:
+      "What is the largest freshwater lake in the world by its area size?",
+    clue: "the largest of the Great Lakes of North America, and among freshwater lakes",
+    answer: "Lake Superior",
+  },
+  {
+    question: "According to George Orwell, who is watching us?",
+    clue: "a fictional character and symbol in George Orwell's dystopian 1949 novel Nineteen Eighty-Four",
+    answer: "Big Brother",
+  },
+  {
+    question: "Which sport takes place in a Velodrome?",
+    clue: "also called biking",
+    answer: "Cycling",
+  },
+  {
+    question: "What is the currency of Poland?",
+    clue: "Polish: gold coin",
+    answer: "Zloty",
+  },
+  {
+    question: "Trainspotting is a novel by which author?",
+    clue: "a Scottish novelist, playwright and short story writer",
+    answer: "Irvine Welsh",
+  },
+  {
+    question:
+      "Which is the only planet in our solar system not named after a Roman or Greek god?",
+    clue: "consists of land, air, water and life",
+    answer: "Earth",
+  },
+  {
+    question: "In which sport is the Davis Cup contested?",
+    clue: "a racket sport",
+    answer: "Tennis",
+  },
+  {
+    question: "What is the name of the daughter in The Addams Family?",
+    clue: "like day of the week",
+    answer: "Wednesday Addams",
+  },
+  {
+    question:
+      "Jacinda Ardern became the Prime Minister of which country in 2017?",
+    clue: "an island country in the southwestern Pacific Ocean",
+    answer: "New Zealand",
+  },
+  {
+    question: "Which magazine has a distinctive yellow border on the cover?",
+    clue: "a popular American monthly magazine",
+    answer: "National Geographic",
+  },
+  {
+    question: "How many wisdom teeth does the average adult have?",
+    clue: "number less than five",
+    answer: "Four",
+  },
+  {
+    question: "What type of rice is risotto usually made from?",
+    clue: "an Italian short-grain rice",
+    answer: "Arborio",
+  },
+  {
+    question: "How many rings is the Olympic symbol made up of?",
+    clue: "number less than six",
+    answer: "Five",
+  },
+  {
+    question: "Which British supermarket has a clothing range named ‘George’?",
+    clue: "much cheaper than Waitrose",
+    answer: "Asda",
+  },
+  {
+    question: "What is a young giraffe called?",
+    clue: "(plural calves) is a young domestic cow or bull",
+    answer: "A calf",
+  },
+  {
+    question: "Copacabana Beach is located in which city?",
+    clue: "the capital of the state of the same name",
+    answer: "Rio de Janeiro",
+  },
+  {
+    question: "Which US agency has the motto “For the benefit of all”?",
+    clue: "government agency that is responsible for science and technology related to air and space",
+    answer: "NASA",
+  },
+  {
+    question: "Pyrophobia is the fear of what?",
+    clue: "a chemical reaction that gives off light and heat",
+    answer: "Fire",
+  },
+  {
+    question: "What is the capital of Argentina?",
+    clue: "argest, and primate city of Argentina",
+    answer: "Buenos Aires",
+  },
+  {
+    question:
+      "A podiatrist specialises in treating which part of the human body?",
+    clue: "contains the five toes",
+    answer: "The feet",
+  },
+  {
+    question: "Which singer had a hit in the 1970s called ‘Maggie May’?",
+    clue: "a British rock and pop singer, songwriter, and record producer",
+    answer: "Rod Stewart",
+  },
+  {
+    question: "Emma Bunton is known as which Spice Girl?",
+    clue: "an English singer and media personality",
+    answer: "Baby Spice",
+  },
+  {
+    question: "What is the name of the winged horse in Greek mythology?",
+    clue: "spyware developed by the Israeli cyber-arms company NSO Group",
+    answer: "Pegasus",
+  },
+  {
+    question: "What is the name of Donald Duck’s girlfriend?",
+    clue: "cartoon character created by The Walt Disney Company",
+    answer: "Daisy Duck",
+  },
+  {
+    question: "‘The Beach’ starring Leonardo DiCaprio is set in which country?",
+    clue: "historically known as Siam",
+    answer: "Thailand",
+  },
+  {
+    question: "Who launched the Cultural Revolution in China?",
+    clue: "was a Chinese communist revolutionary",
+    answer: "Chairman Mao",
+  },
+];
+
+let number;
+let rightAnswer;
+let rightAnswerArr;
+let hiddenAnswer;
+let hiddenAnswerArr;
+let answerPrg = document.querySelector("#answer");
+let cluePrg = document.querySelector("#clue");
+let live;
+let flag;
+let restLives = document.querySelector("#lives");
+
+function getRandomInt(max) {
+  number = Math.floor(Math.random() * max);
+}
+
+function startQuiz() {
+  cluePrg.innerHTML = "click hint to get a clue";
+  live = 9;
+  restLives.innerHTML = `You have ${live} lives`;
+
+  getRandomInt(arr.length);
+
+  let questionPrg = document.querySelector("#question");
+  questionPrg.innerHTML = arr[number].question;
+
+  rightAnswer = arr[number].answer;
+  rightAnswerArr = rightAnswer.split("");
+
+  hiddenAnswer = arr[number].answer;
+  hiddenAnswerArr = hiddenAnswer.split("");
+  for (let i = 0; i < hiddenAnswerArr.length; i++) {
+    if (hiddenAnswerArr[i] != " ") {
+      hiddenAnswerArr[i] = "_";
+    }
+  }
+
+  answerPrg.innerHTML = hiddenAnswerArr.join("");
+}
+
+function showClue() {
+  cluePrg.innerHTML = arr[number].clue;
+}
+
+function playAgain() {
+  location.reload();
+}
+
+function checkLetter(event) {
+  if (event.target.className == "letter") {
+    event.target.classList.remove("letter");
+    event.target.classList.add("btn");
+    flag = false;
+
+    for (let i = 0; i < rightAnswerArr.length; i++) {
+      if (event.target.innerHTML == rightAnswerArr[i].toLowerCase()) {
+        hiddenAnswerArr[i] = rightAnswerArr[i];
+        answerPrg.innerHTML = hiddenAnswerArr.join("");
+        flag = true;
+      }
+    }
+
+    if (hiddenAnswerArr.toString() == rightAnswerArr.toString()) {
+      let result = document.querySelector("#result");
+      let win = document.querySelector(".win");
+      result.style.display = "block";
+      win.style.display = "block";
+    }
+
+    if (flag == false) {
+      live--;
+      restLives.innerHTML = `You have ${live} lives`;
+      if (live < 1) {
+        restLives.innerHTML = `Game over`;
+        let result = document.querySelector("#result");
+        let lose = document.querySelector(".lose");
+        result.style.display = "block";
+        lose.style.display = "block";
+      }
+    }
+  }
+}
+
+let letterBtn = document.querySelector("#alphabet");
+letterBtn.addEventListener("click", checkLetter);
+
+startQuiz();
